9c91e3ebbc749caecefb01bf4e302ac8032cba4f
GEEKiDoS <geek_ds@foxmail.com>
2025-06-18 21:24:08 +0800

    add hachimi editor

---
+++ hachimi/precache-manifest.4f3369f87fd47ab3300a25fed5653e2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cad4c845d80fb6abc8941a6ffebf55d",
+    "url": "/hachimi/index.html"
+  },
+  {
+    "revision": "894cab2f488d4c1831ce",
+    "url": "/hachimi/static/css/main.5238747f.chunk.css"
+  },
+  {
+    "revision": "557b6b6f32b390f7b18f",
+    "url": "/hachimi/static/js/2.b19e9550.chunk.js"
+  },
+  {
+    "revision": "8b055075f934c3b14d1bd5564a7a7891",
+    "url": "/hachimi/static/js/2.b19e9550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894cab2f488d4c1831ce",
+    "url": "/hachimi/static/js/main.7e8c3124.chunk.js"
+  },
+  {
+    "revision": "8b98113608dc2ce29129",
+    "url": "/hachimi/static/js/runtime-main.7652788c.js"
+  }
+]);
